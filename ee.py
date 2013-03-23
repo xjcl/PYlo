6d4596d92c827d8436d839a989c47e3b6e56db7c
Jan@ubuntu <jan@ubuntu.ubuntu-domain>
2013-03-24 00:15:14 +0100

    added feature: you can now type in 'pass'

--- ee.py
+++ ee.py
@@ -3,27 +3,48 @@ import phylo
 import event
 import phylo_dic
 import event_dic
+import home
 from random import shuffle
 
 class Entity(object):
-    def __init__(self, pinfo, pdeck): # deck as list of (common) names
+    class Deck(object):
+        def __init__(self, acont, phci):
+            self.cont = acont
+            self.hc = home.Home(phci) # is initing with strings such a good idea? idk.
+            
+        def get_size(self):
+            return len(self.cont)    
+            
+        def append(self, pl):
+            self.cont.append(pl)
+    
+        def shuffle(self): # um... own deck class?
+            shuffle(self.deck)
+            
+        def get_home_card(self):
+            return self.hc
+    
+    
+    
+    def __init__(self, pinfo, pdeck, phci, ptype):
         self.name = pinfo
-        self.deck = []
+        self.deck = Entity.Deck(pdeck, phci)
         self.moves_left = 0
+        self.type = ptype # 0: human # 1: dumb AI
         for a_card_name in pdeck:
             if phylo_dic.get(a_card_name):
                 self.deck.append(phylo.Phylo(a_card_name))
             if event_dic.get(a_card_name): # in case there are terrain cards or stuff
                 self.deck.append(event.Event(a_card_name))
+    
+    def get_type(self):
+        return self.type    
         
     def get_name(self):
         return self.name
         
     def get_deck(self):
         return self.deck
-        
-    def shuffle_deck(self): # um... own deck class?
-        shuffle(self.deck)
         
     def get_moves_left(self):
         return self.moves_left
